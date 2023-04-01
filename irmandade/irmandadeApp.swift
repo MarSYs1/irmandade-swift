ca46d5b3f55cd02290c95237e91c678a6573c881
Mauro de Oliveira Andre <mauroolivere@hotmail.com>
2023-04-01 19:28:52 +0100

    Initial Commit

---
+++ irmandade/irmandadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  irmandadeApp.swift
+//  irmandade
+//
+//  Created by Mauro de Oliveira  Andre on 01/04/23.
+//
+
+import SwiftUI
+
+@main
+struct irmandadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
